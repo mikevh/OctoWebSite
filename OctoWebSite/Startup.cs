797a529bc893a0e1c9612a0b4ae4c4eb4826b881
Michael VanderHelm <michaelv@attunix.com>
2015-06-19 12:35:58 -0700

    initial

---
+++ OctoWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OctoWebSite.Startup))]
+namespace OctoWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
